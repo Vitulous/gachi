ddf5cf46486d5bc673fc37ee52c88d9c8b235adc
Vitulous <44551327+Vitulous@users.noreply.github.com>
2019-01-13 00:57:30 +0200

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -10,7 +10,7 @@ client = discord.Client()
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.author.id == '533708296956280832'
+    if message.author.id == 533708296956280832:
         msg = 'ты хуй, а я хороший бот'.format(message)
     tmpsg = message.content
     message.content = message.content.lower()
