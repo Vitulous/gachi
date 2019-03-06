a5b30d1e23451b303fad4885a0b95cee6bad643c
Vitulous <44551327+Vitulous@users.noreply.github.com>
2019-03-06 14:39:02 +0200

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -5,7 +5,7 @@ import re
 import asyncio
 import cv2
 from googletrans import Translator
-
+from moviepy.editor import *
 
 translator = Translator()
 langs = ("af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-cn", "zh-tw", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu", "fil", "he")
@@ -134,18 +134,13 @@ async def on_message(message):
         return
     
     elif message.content.startswith('--мошимоши'):
-        cap = cv2.VideoCapture('ravu.mp4')
-        raframe = random.randint(1, 134)
-        raframe = raframe * 1000
-        for i in range(10):
-            cap.set(cv2.CAP_PROP_POS_MSEC, raframe)
-            ret,frame = cap.read()            
-            cv2.imwrite("sekkusu.png", frame)
-            if i == 0: remsg = await client.send_file(message.channel, 'sekkusu.png')
-            else:
-                asyncio.sleep(1)
-                raframe += 1000
-                await client.edit_message(remsg, new_content=send_file('sekkusu.png'))
+        ranstart = random.randint(1, 140)
+        ranend = ranstart + 3
+        clip = (VideoFileClip('ravu.mp4')
+        .subclip(ranstart, ranend)
+        .resize(0.5))
+        clip.write_gif("sekkusu.gif")
+        await client.send_file(message.channel, 'sekkusu.gif')
         return
     
     elif message.content.startswith('--отомсти'):
