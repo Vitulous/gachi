4c24fc83e0e9908aa05d3ba0571f7af675e47032
Vitulous <44551327+Vitulous@users.noreply.github.com>
2019-03-06 14:04:30 +0200

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -145,7 +145,7 @@ async def on_message(message):
             else:
                 asyncio.sleep(1)
                 raframe += 1000
-                await client.edit_message(remsg, 'sekkusu.png')
+                await client.edit_message(remsg, embed='sekkusu.png')
         return
     
     elif message.content.startswith('--отомсти'):
