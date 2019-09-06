252cf057a194f245e7c4856dca6718be4c09a65d
Vitulous <44551327+Vitulous@users.noreply.github.com>
2019-09-06 17:10:20 +0300

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -388,7 +388,7 @@ async def on_message(message):
 
     if '--ответь' in message.content:
       if message.author.id == '314363965125820417':
-        numb = re.search('\d+', message.content).group()
+        numb = re.search('\d+', message.content)
         numb = int(numb)
         if numb >= 49:
             await client.send_message(message.channel, 'иди нахуй')
