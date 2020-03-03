079ed6561c8bdd174af6037941d4312464553265
Vitulous <44551327+Vitulous@users.noreply.github.com>
2020-03-03 16:48:10 +0200

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -11,6 +11,8 @@ from moviepy.editor import *
 translator = Translator()
 langs = ("af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-cn", "zh-tw", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu", "fil", "he")
 
+pi = '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
+
 response = google_images_download.googleimagesdownload()
 
 client = discord.Client()
@@ -164,7 +166,16 @@ async def on_message(message):
         cv2.imwrite("revenge.png", frame)
         await client.send_file(message.channel, 'revenge.png')
         return
-        
+
+    elif message.content.startswith('--оцени'):
+        s = len(tmpsg[8:])
+        if s > 100:
+            msg = 'иди нахуй'.format(message)
+        else:
+            rank = pi[s]
+            if rank = '0': rank = '10'
+            msg = (rank + '/10').format(message)
+    
     elif message.content.startswith('--ищи'):
       if len(tmpsg) > 40:
         await client.send_message(message.channel, 'иди нахуй')
