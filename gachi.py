731769292f0e3a48b5ebe39db1e2d11d8368798e
Vitulous <44551327+Vitulous@users.noreply.github.com>
2020-11-29 15:01:12 +0200

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -9,7 +9,6 @@ from google_images_download import google_images_download
 from moviepy.editor import *
 
 translator = google_translator()
-dest = lang_tgt
 langs = ("af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-cn", "zh-tw", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu", "fil", "he")
 
 pi = '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
@@ -114,18 +113,18 @@ async def on_message(message):
         if tmpsg[7] == '(' and tmpsg[10] == ')':
             langcheck = tmpsg[8] + tmpsg[9]
             if langcheck in langs:
-                ttext = translator.translate(tmpsg[12:], dest=langcheck).text
-            else: ttext = translator.translate(tmpsg[12:], dest='ru').text
-        else: ttext = translator.translate(tmpsg[8:], dest='ru').text
+                ttext = translator.translate(tmpsg[12:], lang_tgt=langcheck).text
+            else: ttext = translator.translate(tmpsg[12:], lang_tgt='ru').text
+        else: ttext = translator.translate(tmpsg[8:], lang_tgt='ru').text
         msg = ttext.format(message)
         
     elif message.content.startswith('--суп'):
         uwu = tmpsg[6:]
         for i in range (10):
             owo = random.choice(langs)
-            ttext = translator.translate(uwu, dest=owo).text
+            ttext = translator.translate(uwu, lang_tgt=owo).text
             uwu = ttext
-        ttext = translator.translate(uwu, dest='ru').text
+        ttext = translator.translate(uwu, lang_tgt='ru').text
         msg = ttext.format(message)
     
     elif message.content.startswith('--мошимоши'):
