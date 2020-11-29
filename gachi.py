ec4a6b8a5e255f874a01d61e346d9660ed33b4a9
Vitulous <44551327+Vitulous@users.noreply.github.com>
2020-11-29 14:50:13 +0200

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -4,11 +4,11 @@ import os
 import re
 import asyncio
 import cv2
-from googletrans import Translator
+from google_trans_new import google_translator  
 from google_images_download import google_images_download 
 from moviepy.editor import *
 
-translator = Translator()
+translator = google_translator()
 langs = ("af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-cn", "zh-tw", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "uz", "vi", "cy", "xh", "yi", "yo", "zu", "fil", "he")
 
 pi = '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
