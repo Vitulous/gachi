fa3891fe033774036e53c6c5db6e7f4fb49233c1
Vitulous <44551327+Vitulous@users.noreply.github.com>
2021-03-24 14:23:56 +0200

    Update gachi.py

--- gachi.py
+++ gachi.py
@@ -93,7 +93,7 @@ async def on_message(message):
                 if tim == 0: remsg = await channel.send(msg)
                 else:
                     await asyncio.sleep(1)
-                    await edit_message(remsg, msg)
+                    await edit(remsg, msg)
             return
         else: msg = 'иди нахуй'.format(message)
             
